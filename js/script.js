e8ced021ca576533489ff0f821f3910824f0bb2d
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-08-22 17:22:13 +0800

    Add constructor for Book object

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function() {
+    return `${title} by ${author}, ${pages} pages, ${read}`;
+  }
+
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
